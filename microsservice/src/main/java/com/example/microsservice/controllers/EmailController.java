20358911cbb0d69171782f9635b6fc36924f7d25
Thaynara <thay-cr@outlook.com>
2021-09-25 21:25:54 -0300

    aplicação enviando os emails

--- microsservice/src/main/java/com/example/microsservice/controllers/EmailController.java
+++ microsservice/src/main/java/com/example/microsservice/controllers/EmailController.java
@@ -1,5 +1,4 @@
 package com.example.microsservice.controllers;
-import javax.validation.Valid;
 import com.example.microsservice.dtos.EmailDto;
 import com.example.microsservice.models.EmailModel;
 import com.example.microsservice.services.EmailService;
@@ -7,22 +6,36 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+
 
 @RestController
 public class EmailController {
 
     @Autowired
-    EmailService emailServEmailService;
+    EmailService emailService;
 
-    @PostMapping("/email")
-    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto){
+    @PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto) {
         EmailModel emailModel = new EmailModel();
         BeanUtils.copyProperties(emailDto, emailModel);
-        EmailService.sendEmail(emailModel);
+        emailService.sendEmail(emailModel);
         return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
     }
-    
+
+    // @GetMapping("/emails")
+    // public ResponseEntity<Page<EmailModel>> getAllEmails(@PageableDefault(page = 0, size = 5, sort = "emailId", direction = Sort.Direction.DESC) Pageable pageable){
+    //     return new ResponseEntity<>(emailService.findAll(pageable), HttpStatus.OK);
+    // }
+
+    // @GetMapping("/emails/{emailId}")
+    // public ResponseEntity<Object> getOneEmail(@PathVariable(value="emailId") UUID emailId){
+    //     Optional<EmailModel> emailModelOptional = emailService.findById(emailId);
+    //     if(!emailModelOptional.isPresent()) {
+    //         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email not found.");
+    //     }else {
+    //         return ResponseEntity.status(HttpStatus.OK).body(emailModelOptional.get());
+    //     }
+    // }
 }
--- microsservice/src/main/java/com/example/microsservice/services/EmailService.java
+++ microsservice/src/main/java/com/example/microsservice/services/EmailService.java
@@ -1,8 +1,15 @@
 package com.example.microsservice.services;
+
+import com.example.microsservice.enums.StatusEmail;
 import com.example.microsservice.models.EmailModel;
 import com.example.microsservice.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -10,7 +17,33 @@ public class EmailService {
     @Autowired
     EmailRepository emailRepository;
 
-    public static void sendEmail(EmailModel emailModel) {
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public EmailModel sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+             emailRepository.save(emailModel);
+        }
+        return emailModel;
     }
-    
+
+    // public Page<EmailModel> findAll(Pageable pageable) {
+    //     return  emailRepository.findAll(pageable);
+    // }
+
+    // public Optional<EmailModel> findById(UUID emailId) {
+    //     return emailRepository.findById(emailId);
+    // }
 }
